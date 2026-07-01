c9a22393bebce4bf8005aa90e886d9f5ae29ba6b
Chris Wolfgang <210299580+Chris-Wolfgang@users.noreply.github.com>
2026-07-01 08:35:08 -0400

    remove unused using System.Threading.Tasks from JsonSingleStreamExtractor

--- src/Wolfgang.Etl.Json/JsonSingleStreamExtractor.cs
+++ src/Wolfgang.Etl.Json/JsonSingleStreamExtractor.cs
@@ -4,7 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using System.Threading;
-using System.Threading.Tasks;
 using System.IO;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
